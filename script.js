cf28c58259b1d98e5c3450eac10517c29a8a38fe
Shreyansh Shikhar Srivastava <133575578+shreyanshshikhar@users.noreply.github.com>
2023-10-07 17:29:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -28,6 +28,51 @@ listContainer.addEventListener("click", function(e) {
     }
 }, false);
 
+const inputBox = document.getElementById('input-box');
+const listContainer = document.getElementById('list-container');
+
+function addTask() {
+    if (inputBox.value === '') {
+        alert("nothing to add,so write first");
+    } else {
+        let li = document.createElement('li');
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement('span');
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+listContainer.addEventListener("click", function(e) {
+    if (e.target.tagName === 'LI') {
+        e.target.classList.toggle("checked");
+        saveData();
+
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+
+    }
+}, false);
+
+inputBox.addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+        addTask();
+    }
+});
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
+
 function saveData() {
     localStorage.setItem("data", listContainer.innerHTML);
 }
